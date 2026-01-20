42e74ea5f67d39ef5248375a0276b634bbe7aa68
timeol21 <lqh15673702008@outlook.com>
2026-01-20 19:02:28 +0800

    加强了烟雾传感器的逻辑

--- contant_code_1/Sensors.cpp
+++ contant_code_1/Sensors.cpp
@@ -31,7 +31,12 @@ void WaterSensor::execute() {
 // --- 温湿度传感器逻辑 ---
 HumitureSensor::HumitureSensor() : currentTemp(0), currentHumi(0) {}
 void HumitureSensor::execute() {
-    if (sm.fd < 0) sm.initPort(portPath);
+    if (sm.fd < 0) {
+        if (!sm.initPort(portPath)) {
+            cout << "[" << type << "] 无法初始化串口 " << portPath << endl;
+            return;
+        }
+    }
     unsigned char cmd[] = {0x01, 0x03, 0x00, 0x00, 0x00, 0x02, 0xC4, 0x0B};
     sm.sendData(cmd, 8);
     unsigned char buf[255];
@@ -41,5 +46,11 @@ void HumitureSensor::execute() {
         currentHumi = ((buf[3] << 8) | buf[4]) / 10.0;
         currentTemp = ((buf[5] << 8) | buf[6]) / 10.0;
         cout << "[" << type << " ID:" << id << "] 温度: " << currentTemp << "C, 湿度: " << currentHumi << "%" << endl;
+    } else if (len == 0) {
+        cout << "[" << type << "] 警告: 传感器无响应 (USB可能已断开，尝试关闭以便下次重连)" << endl;
+        sm.closePort(); // 读不到数据时主动关闭，下次 execute 会触发重新 initPort
+    } else {
+        // 如果没读到数据，也打印出来
+        cout << "[" << type << " ID:" << id << "] 串口无响应或长度不对 (收到 " << len << " 字节)" << endl;
     }
 }
