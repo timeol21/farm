db8e3d02be6e0225bcee63dc1238343195b19c2c
timeol21 <lqh15673702008@outlook.com>
2026-01-29 20:43:38 +0800

    继续上传部分雷达的文件

---
+++ boxfiles/Radar_lqh/unilidar_sdk/unitree_lidar_sdk/build/CMakeFiles/unilidar_publisher_udp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/unilidar_publisher_udp"
+  "../bin/unilidar_publisher_udp.pdb"
+  "CMakeFiles/unilidar_publisher_udp.dir/examples/unilidar_publisher_udp.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unilidar_publisher_udp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ boxfiles/Radar_lqh/unilidar_sdk/unitree_lidar_sdk/build/CMakeFiles/unilidar_subscriber_udp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/unilidar_subscriber_udp"
+  "../bin/unilidar_subscriber_udp.pdb"
+  "CMakeFiles/unilidar_subscriber_udp.dir/examples/unilidar_subscriber_udp.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unilidar_subscriber_udp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
